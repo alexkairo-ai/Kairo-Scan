540d86b33300e8087265d845db3b8131d5f12b5d
alexkairo-ai <alexkairo@yandex.ru>
2026-02-11 12:43:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -677,3 +677,11 @@ editReportsBtn.onclick=()=>{
 };
 
 document.getElementById('refreshBtn').onclick = () => location.reload(true);
+
+/* ====== регистрация SW для авто-обновлений ====== */
+if ('serviceWorker' in navigator) {
+ navigator.serviceWorker.register('/sw.js');
+ navigator.serviceWorker.addEventListener('controllerchange', () => {
+ location.reload();
+ });
+}
